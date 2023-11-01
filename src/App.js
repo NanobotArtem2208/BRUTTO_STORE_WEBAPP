f2d4ad622bbe8d65202604cd813a27639a3df0c3
НаноботАртем2208 <astramoda30@gmail.com>
2023-11-02 00:17:40 +0400

    2.0_Web

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
 import './App.css';
-
-const tg = window.Telegram.WebApp;
-
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Admin from "./components/Admin/Admin";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
-  useEffect( () => {
-    tg.ready();
-  }, [])
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
-
-  return (
-    <div className="App">
-      <button onClick={onClose}>Закрыть</button>
-      <span className={'username'}>
-        {tg.initDataUnsafe?.user?.username}
-      </span>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'Admin'} element={<Admin />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
